3aa816d841cce4a77ed577e4af9f6527da0c3913
ahsai001 <ahsai001@gmail.com>
2024-02-01 15:30:37 +0700

    release to v0.1.2

--- lib/src/utils/widget_util.dart
+++ lib/src/utils/widget_util.dart
@@ -488,32 +488,30 @@ Future<void> showStatefullWidgetsInDialog<I extends Object, O extends Object>(
   await showGeneralDialog(
       context: context,
       barrierDismissible: false,
-      // transitionBuilder: (context, animation, secondaryAnimation, child) {
-      //   const begin = Offset(0.0, 1.0);
-      //   const end = Offset.zero;
-      //   const curve = Curves.ease;
+      transitionBuilder: (context, animation, secondaryAnimation, child) {
+        const begin = Offset(0.0, 1.0);
+        const end = Offset.zero;
+        const curve = Curves.ease;
 
-      //   final tween = Tween(begin: begin, end: end);
-      //   final curvedAnimation = CurvedAnimation(
-      //     parent: animation,
-      //     curve: curve,
-      //   );
+        final tween = Tween(begin: begin, end: end);
+        final curvedAnimation = CurvedAnimation(
+          parent: animation,
+          curve: curve,
+        );
 
-      //   return SlideTransition(
-      //     position: tween.animate(curvedAnimation),
-      //     child: child,
-      //   );
-      // },
+        return SlideTransition(
+          position: tween.animate(curvedAnimation),
+          child: child,
+        );
+      },
       pageBuilder: (BuildContext context, animIn, animOut) {
         I? request = getRequest?.call(context);
         return CustomLightThemeWidget(
           modifyTheme: modifyTheme,
           child: StatefulBuilder(builder: (context, setState) {
             final ScrollController controller = ScrollController();
-            return WillPopScope(
-              onWillPop: () {
-                return Future.value(false);
-              },
+            return PopScope(
+              canPop: false,
               child: AlertDialog(
                 title: AppBar(
                   title: titleBuilder != null
