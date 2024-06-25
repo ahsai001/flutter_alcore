8dbb3c11d1e07d275d7a5abd190bcd0b544b5d89
ahsai001 <ahsai001@gmail.com>
2024-06-25 15:15:33 +0700

    flutter alcore support web

--- lib/src/data/database/drift_provider.dart
+++ lib/src/data/database/drift_provider.dart
@@ -1,9 +1,5 @@
-import 'dart:io';
-
 import 'package:drift/drift.dart';
-import 'package:drift/native.dart';
-import 'package:path/path.dart' as p;
-import 'package:path_provider/path_provider.dart';
+import 'package:drift/wasm.dart';
 
 part 'drift_provider.g.dart';
 
@@ -47,9 +43,24 @@ class DriftProvider extends _$DriftProvider {
 }
 
 LazyDatabase _openConnection() {
+  // return LazyDatabase(() async {
+  //   final dbFolder = await getApplicationDocumentsDirectory();
+  //   final file = File(p.join(dbFolder.path, 'db.sqlite'));
+  //   return NativeDatabase.createInBackground(file);
+  // });
+
   return LazyDatabase(() async {
-    final dbFolder = await getApplicationDocumentsDirectory();
-    final file = File(p.join(dbFolder.path, 'db.sqlite'));
-    return NativeDatabase.createInBackground(file);
+    final result = await WasmDatabase.open(
+      databaseName: 'my_app_db', // prefer to only use valid identifiers here
+      sqlite3Uri: Uri.parse('sqlite3.wasm'),
+      driftWorkerUri: Uri.parse('drift_worker.dart.js'),
+    );
+
+    if (result.missingFeatures.isNotEmpty) {
+      print('Using ${result.chosenImplementation} due to missing browser '
+          'features: ${result.missingFeatures}');
+    }
+
+    return result.resolvedExecutor;
   });
 }
